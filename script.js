6bc287c4bb72c190f1d1b47da42a7e0aeed20e67
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2024-08-24 07:01:41 +0530

    FInal

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.from('h1', {
+    opacity:0,
+    duration:2,
+    dalay:2,
+    repeat:-1,
+    backgroundColor:"red",
+    color:"blue",
+    rotate:360,
+    border:5,
+    borderRadius:"50%",
+    scale:0.4
+})
